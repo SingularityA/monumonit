aab12b4522356f0c1ef22ee2dbc9ded24d06d4a8
SingularityA <singularity.m101@gmail.com>
2018-04-17 19:45:15 +0300

    Add main app config

---
+++ src/main/java/com/monumonit/config/AppConfig.java
@@ -0,0 +1,10 @@
+package com.monumonit.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.monumonit")
+public class AppConfig {
+
+}
