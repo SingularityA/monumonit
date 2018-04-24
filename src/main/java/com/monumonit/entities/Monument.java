abe015276b4eee96d40e46031aead20973f4004a
SingularityA <singularity.m101@gmail.com>
2018-04-24 23:08:08 +0300

    Update Monument entity

--- src/main/java/com/monumonit/entities/Monument.java
+++ src/main/java/com/monumonit/entities/Monument.java
@@ -2,13 +2,16 @@ package com.monumonit.entities;
 
 import com.monumonit.entities.interfaces.RecursiveEntityInterface;
 import lombok.Data;
+import org.hibernate.annotations.DynamicUpdate;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
 @Entity
+@DynamicUpdate
 public class Monument implements Serializable, RecursiveEntityInterface<Monument> {
 
     @Id
@@ -18,13 +21,13 @@ public class Monument implements Serializable, RecursiveEntityInterface<Monument
 
     String name;
 
-    @ManyToOne
-    @JoinColumn(name = "monument_type_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "monument_type_id", nullable = false)
     MonumentType monumentType;
 
     String description;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "complex_id")
     Complex complex;
 
@@ -32,6 +35,20 @@ public class Monument implements Serializable, RecursiveEntityInterface<Monument
     @JoinColumn(name = "part_monument_id")
     Monument parent;
 
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "parent")
+    @OneToMany(mappedBy = "parent",
+            cascade = {CascadeType.ALL},
+            fetch = FetchType.LAZY)
     List<Monument> children;
+
+    public void adopt(List<Monument> children) {
+        children.forEach(child -> child.setParent(this));
+        this.children.addAll(children);
+    }
+
+    public void transferChildrenTo(Monument parent) {
+        this.children.forEach(child -> child.setParent(parent));
+        if (parent != null)
+            parent.getChildren().addAll(children);
+        this.children = new ArrayList<>();
+    }
 }
