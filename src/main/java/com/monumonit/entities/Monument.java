99c2647ce87246568f9197c8b80f9e0f28afba5d
SingularityA <singularity.m101@gmail.com>
2018-04-24 03:10:02 +0300

    Monument implements Recursive interface

--- src/main/java/com/monumonit/entities/Monument.java
+++ src/main/java/com/monumonit/entities/Monument.java
@@ -1,13 +1,15 @@
 package com.monumonit.entities;
 
+import com.monumonit.entities.interfaces.RecursiveEntityInterface;
 import lombok.Data;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.List;
 
 @Data
 @Entity
-public class Monument implements Serializable {
+public class Monument implements Serializable, RecursiveEntityInterface<Monument> {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,7 +18,20 @@ public class Monument implements Serializable {
 
     String name;
 
-    Long complexId;
+    @ManyToOne
+    @JoinColumn(name = "monument_type_id")
+    MonumentType monumentType;
 
-    Long partMonumentId;
+    String description;
+
+    @ManyToOne
+    @JoinColumn(name = "complex_id")
+    Complex complex;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "part_monument_id")
+    Monument parent;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "parent")
+    List<Monument> children;
 }
