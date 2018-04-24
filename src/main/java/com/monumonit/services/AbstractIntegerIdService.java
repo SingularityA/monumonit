9dc3d6b184799bcf62e873be2254881768043905
SingularityA <singularity.m101@gmail.com>
2018-04-24 03:13:58 +0300

    Add abstract service for integer id entities

---
+++ src/main/java/com/monumonit/services/AbstractIntegerIdService.java
@@ -0,0 +1,19 @@
+package com.monumonit.services;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.io.Serializable;
+import java.util.Optional;
+
+@Transactional
+public abstract class AbstractIntegerIdService<T extends Serializable> extends AbstractBasicService<T, Integer> {
+    protected abstract JpaRepository<T, Integer> getRepository();
+
+    @Override
+    @Transactional(readOnly = true)
+    public T find(final Integer id) {
+        Optional<T> entity = getRepository().findById(id);
+        return entity.orElse(null);
+    }
+}
