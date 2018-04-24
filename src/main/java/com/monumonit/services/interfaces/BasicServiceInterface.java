baf79bb7a5ab18a69fede9c8ced19bb8d5577e0c
SingularityA <singularity.m101@gmail.com>
2018-04-24 03:11:52 +0300

    Basic service interface updated: generic ID added

--- src/main/java/com/monumonit/services/interfaces/BasicServiceInterface.java
+++ src/main/java/com/monumonit/services/interfaces/BasicServiceInterface.java
@@ -3,13 +3,17 @@ package com.monumonit.services.interfaces;
 import java.io.Serializable;
 import java.util.List;
 
-public interface BasicServiceInterface<T extends Serializable> {
+public interface BasicServiceInterface<T extends Serializable, ID_TYPE> {
 
     List<T> findAll();
 
-    T find(final Long id);
+    T find(final ID_TYPE id);
 
     T save(final T entity);
+
+    T saveAndFlush(final T entity);
+
+    List<T> saveAll(final Iterable<T> entities);
 
     void delete(final T entity);
 }
