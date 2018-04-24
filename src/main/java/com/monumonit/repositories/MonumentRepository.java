445faf774b38744ac98bd310103921d5f471963e
SingularityA <singularity.m101@gmail.com>
2018-04-24 03:00:18 +0300

    Move from Spring to Spring Boot (for more simple web setup)

--- src/main/java/com/monumonit/repositories/MonumentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.monumonit.repositories;
-
-import com.monumonit.entities.Monument;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MonumentRepository extends JpaRepository<Monument, Long> {
-}
--- src/main/java/com/monumonit/services/interfaces/MonumentServiceInterface.java
+++
@@ -1,6 +0,0 @@
-package com.monumonit.services.interfaces;
-
-import com.monumonit.entities.Monument;
-
-public interface MonumentServiceInterface extends BasicServiceInterface<Monument> {
-}
